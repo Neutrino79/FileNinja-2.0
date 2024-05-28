5aab93e25997881cd24cf45d09085834c5e30d3e
prathameshkhade <pkhade2865@gmail.com>
2024-05-28 08:16:50 +0000

    mv: template to homepage

--- src/FileNinja/urls.py
+++ src/FileNinja/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import home_view
 
 from django.conf import settings
@@ -23,5 +23,5 @@ from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name='home'),
+    path('', include("src.apps.homepage.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
