aa25e3fe20fa243243d4fec32d1f5224714d2b01
Neutrino79 <atharvhiremath.ah@gmail.com>
2024-05-31 13:16:50 +0530

    remove - touch listener for moibile mode

--- src/static/assets/custom/js/customjs.js
+++ src/static/assets/custom/js/customjs.js
@@ -137,11 +137,6 @@ function enableDragAndDrop(fileDiv) {
     fileDiv.addEventListener('dragstart', dragStart);
     fileDiv.addEventListener('dragover', dragOver);
     fileDiv.addEventListener('drop', drop);
-
-    // For touch devices
-    fileDiv.addEventListener('touchstart', touchStart, false);
-    fileDiv.addEventListener('touchmove', touchMove, false);
-    fileDiv.addEventListener('touchend', touchEnd, false);
 }
 
 let draggedElement;
@@ -171,11 +166,11 @@ function dragOver(e) {
         } else {
             dropzone.parentNode.insertBefore(draggedElement, dropzone);
         }
-        applyFadeEffect(fileItems, dropzone, nextSibling);
+        applyFadeEffect(fileItems, dropzone);
     }
 }
 
-function applyFadeEffect(fileItems, dropzone, nextSibling) {
+function applyFadeEffect(fileItems, dropzone) {
     fileItems.forEach(item => {
         if (item !== draggedElement) {
             item.style.opacity = '0.5';
@@ -183,7 +178,6 @@ function applyFadeEffect(fileItems, dropzone, nextSibling) {
     });
     dropzone.style.opacity = '1';
 }
-
 
 function drop(e) {
     e.preventDefault();
@@ -213,59 +207,4 @@ function getNextSibling(el) {
         nextSibling = nextSibling.nextElementSibling;
     }
     return nextSibling;
-}
-
-
-
-// Touch events for mobile drag-and-drop with touch-and-hold
-let touchDragElement = null;
-let touchHoldTimeout = null;
-
-function touchStart(e) {
-    touchDragElement = e.target.closest('.file-item');
-    if (touchDragElement) {
-        touchHoldTimeout = setTimeout(() => {
-            touchDragElement.classList.add('dragging');
-            const touch = e.touches[0];
-            touchDragElement.style.position = 'absolute';
-            touchDragElement.style.zIndex = '1000';
-            moveAt(touch.pageX, touch.pageY);
-        }, 500); // 500ms hold time to initiate drag
-
-        const placeholder = document.createElement('div');
-        placeholder.className = 'placeholder';
-        touchDragElement.insertAdjacentElement('beforebegin', placeholder);
-    }
-}
-
-function touchMove(e) {
-    if (touchDragElement && touchHoldTimeout) {
-        const touch = e.touches[0];
-        moveAt(touch.pageX, touch.pageY);
-        const dropzone = document.elementFromPoint(touch.clientX, touch.clientY).closest('.file-item');
-
-        if (dropzone && dropzone !== touchDragElement) {
-            const placeholder = document.querySelector('.placeholder');
-            dropzone.insertAdjacentElement('beforebegin', placeholder);
-        }
-    }
-}
-
-function touchEnd() {
-    if (touchDragElement && touchHoldTimeout) {
-        clearTimeout(touchHoldTimeout);
-        touchDragElement.classList.remove('dragging');
-        touchDragElement.style.position = '';
-        touchDragElement.style.zIndex = '';
-        const placeholder = document.querySelector('.placeholder');
-        placeholder.insertAdjacentElement('beforebegin', touchDragElement);
-        placeholder.remove();
-        touchDragElement = null;
-        touchHoldTimeout = null;
-    }
-}
-
-function moveAt(pageX, pageY) {
-    touchDragElement.style.left = pageX - touchDragElement.offsetWidth / 2 + 'px';
-    touchDragElement.style.top = pageY - touchDragElement.offsetHeight / 2 + 'px';
 }
